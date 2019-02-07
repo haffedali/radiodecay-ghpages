409b2e00c5fb6414168969f3a6e8deacb4b261c6
Hoff <haffedmcnair@gmail.com>
2019-02-06 17:38:43 -0800

    Updates

---
+++ precache-manifest.59987c342218de05a140e7ffce053a45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0107ff33e6a0ee9117442a542a3ab780",
+    "url": "/[your-user-name].github.io/[your-repo-name]/static/media/atom.0107ff33.png"
+  },
+  {
+    "revision": "78ed2a84c1f2f6169e429631248a1cd4",
+    "url": "/[your-user-name].github.io/[your-repo-name]/static/media/atom-orange.78ed2a84.png"
+  },
+  {
+    "revision": "df0e33623a8704241f20",
+    "url": "/[your-user-name].github.io/[your-repo-name]/static/js/runtime~main.df0e3362.js"
+  },
+  {
+    "revision": "c89ae530bc2c6100d007",
+    "url": "/[your-user-name].github.io/[your-repo-name]/static/js/main.c89ae530.chunk.js"
+  },
+  {
+    "revision": "f4e4e91d8f7753919478",
+    "url": "/[your-user-name].github.io/[your-repo-name]/static/js/1.f4e4e91d.chunk.js"
+  },
+  {
+    "revision": "c89ae530bc2c6100d007",
+    "url": "/[your-user-name].github.io/[your-repo-name]/static/css/main.14a08f37.chunk.css"
+  },
+  {
+    "revision": "60a817ec9c45e1c40aa8bcabd1c50477",
+    "url": "/[your-user-name].github.io/[your-repo-name]/index.html"
+  }
+];
